0010daa93f0d4d8c7513c78c5cb249820d2883f8
timhams <timtam54@hotmail.com>
2025-07-05 11:34:00 +1000

    .

--- app/PoolGameComponent.tsx
+++ app/PoolGameComponent.tsx
@@ -504,7 +504,7 @@ export default function PoolGameComponent() {
 
   return (
     <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center p-4">
-      <h1 className="text-4xl font-bold text-white mb-8"><a href="https://www.AussieSoft.com.au">Stephanie</a> 8-Ball Pool</h1>
+      <h1 className="text-4xl font-bold text-white mb-8"><a href="https://www.AussieSoft.com.au">Onni</a> 8-Ball Pool</h1>
       
       <div className="relative bg-gray-800 p-4 rounded-lg shadow-2xl">
         <canvas
