6bb78c9185e793dd97a5d2c35c213d41ec3976c3
timhams <timtam54@hotmail.com>
2025-07-21 00:00:04 +1000

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.onnitetrisgames.app',
-  appName: 'Onni Tetris',
+  appName: 'Quadrix',
   webDir: 'out',
   server: {
     androidScheme: 'https',
